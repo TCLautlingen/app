2f0d64c1cab47feba6f0cca621ac967dd02bd6d8
Kazox61 <pawelka00florian@gmail.com>
2026-04-30 09:52:34 +0200

    Fix domain.

--- composeApp/src/commonMain/kotlin/org/tcl/app/core/data/network/BackendEndpoints.kt
+++ composeApp/src/commonMain/kotlin/org/tcl/app/core/data/network/BackendEndpoints.kt
@@ -1,5 +1,4 @@
 package org.tcl.app.core.data.network
 
-const val BACKEND_ORIGIN = "http://82.165.106.91"
-// Purchase or configure a domain; add an A record pointing to 82.165.106.91.
+const val BACKEND_ORIGIN = "https://api.tc-lautlingen.de"
 const val BACKEND_BASE_URL = "$BACKEND_ORIGIN/v1/"
